8016f69911154bbbe2a79636c2bea616d848207d
trish <pmgriego@gmail.com>
2021-07-07 10:25:17 -0700

    remove logs etc

--- src/Temperature.js
+++ src/Temperature.js
@@ -2,22 +2,39 @@ import './Temperature.css';
 import { useState } from 'react';
 
 const Temperature = () => {
-    const [tempCount, setTempCount] = useState(0);
     
-    const increaseTemperature = () => {
-        setTempCount(tempCount + 1);
+    const [tempCount, setTempCount] = useState(70);
 
+    const pickTempColor = () => {
+        if (tempCount >= 90) {
+            return "blinking-red"
+        }
+        else if (tempCount >= 80) {
+            return "red";
+        } else if (tempCount >= 70) {
+            return "orange";
+        } else if (tempCount >= 60) {
+            return "yellow";
+        } else if (tempCount >= 50) {
+            return "green";
+        } else {
+            return "teal";
+        }
+    }
+
+    const increaseTemperature = () => {
+        setTempCount(tempCount + 1)
     }
     const decreaseTemperature = () => {
         setTempCount(tempCount - 1);
-
     }
 
+    const color = pickTempColor();
     return (
-        <section>
+        <section className="weather-box">
             <h1 className="center">Temperature</h1>
-            <p className="center">{tempCount}</p>
-            <div className="center">
+            <p className={`${color} center`}>{tempCount}°</p>
+            <div className="center button-holder">
                 <button onClick={increaseTemperature}>➕</button>
                 <button onClick={decreaseTemperature}>➖</button>
             </div>
